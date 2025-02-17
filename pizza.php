9e1102d57a217009292153f65cf516ff15d91b4a
ircacu <467220.clot@fje.edu>
2025-02-17 13:51:57 +0000

    Commit 5.2 de l'aplicatiu pizzes

--- pizza.php
+++ pizza.php
@@ -8,22 +8,24 @@
           $preu = 8;
           $IVA=21;
           if (empty($_GET['ingr'])) {
-               $pvp=((100+$IVA)/100)*$preu;
-    echo "Has demanat només la pizza bàsica. El preu és $pvp € (IVA inclòs)<br>";
-          }
+            $pvp=((100+$IVA)/100)*$preu;
+            echo "Has demanat només la pizza bàsica. El preu és $pvp € (IVA inclòs)<br>";
+            #exit(0);
+	  }
           else {
-               $IVA=21;
-    $pvp=((100+$IVA)/100)*($preu+count($_GET['ingr']));
-     echo "Has demanat la pizza bàsica amb:<br> ";
-     foreach ($_GET['ingr'] as $ingredient) {
-          if ($ingredient) {
-        echo "* $ingredient<br>";
-   }
-      }
-      echo "El preu és $pvp € (IVA inclòs)<br>";
- }
- echo "<br>";
- echo "Autor: ircacu"."<br>";
+            $IVA=21;
+            $pvp=((100+$IVA)/100)*($preu+count($_GET['ingr']));
+            echo "Has demanat la pizza bàsica amb:<br> ";
+            foreach ($_GET['ingr'] as $ingredient) {
+              if ($ingredient) {
+                echo "* $ingredient<br>";
+              }
+          }
+          echo "El preu és $pvp € (IVA inclòs)<br>";
+          #exit(0);
+          }
+          echo "<br>";
+          echo "Autor: ircacu"."<br>";
      ?>
      <a href="pizza.html">Retorna al formulari de selecció d’ingredients</a>
      </body>
